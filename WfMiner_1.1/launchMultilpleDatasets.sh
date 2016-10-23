2d2d26ea3c8898b722c02e68aa2233fc14b47bca
Ahmed Halioui <ahlioui@gmail.com>
2016-10-23 15:49:10 -0400

    Recommender
    Recommender component is added

--- WfMiner_1.1/launchMultilpleDatasets.sh
+++ WfMiner_1.1/launchMultilpleDatasets.sh
@@ -9,11 +9,12 @@ namespace="http://www.co-ode.org/ontologies/ont.owl#"
 gold="./WD-Phy-gold-1.xml"
 topnItems="10"
 topkRules="50"
+minLevel="2"
 
 
 for f in $FILES
 do
   echo "$(basename $f)"
   # take action on each file. $f store current file name
-  java -jar $jarF $minSupp $bowl $f $namespace $gold $topnItems $topnItems
+  java -jar $jarF $minSupp $bowl $f $namespace $gold $topnItems $topnItems $minLevel
 done
