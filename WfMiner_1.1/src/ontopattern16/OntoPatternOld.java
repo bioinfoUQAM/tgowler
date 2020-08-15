5ec86da901093166ca85e3651293e0512b921bbf
ahmedhalioui <ahmed@mims.ai>
2020-08-15 10:15:50 -0400

    trying to fix intellij - 1

--- WfMiner_1.1/src/ontopattern16/OntoPatternOld.java
+++ WfMiner_1.1/src/ontopattern16/OntoPatternOld.java
@@ -147,7 +147,7 @@ public class OntoPatternOld {
 
 //            System.out.println("Reading the Training set " + parametres.get(mode)[1] + " ...");
             loadRawSequences(rawUserSequences, userSequences, ontology, rawSequences, localNameNode, args[3]);
-            System.out.println("rawUserWorkflows: " + rawUserSequences.toString());
+//            System.out.println("rawUserWorkflows: " + rawUserSequences.toString());
             // test set
 //            System.out.println("Reading the Test set " + parametres.get(mode)[3] + " ...");
             loadRawSequences(rawUserGoldSequences, userGoldSequences, ontology, rawGoldSequences, localNameNode, args[3]);
